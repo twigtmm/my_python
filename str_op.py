dda417db8ab0f73f7917b3b3a65b583d94e7dd67
Darya Popova <dasha_popova_1999@mail.ru>
2019-09-18 23:05:09 +0300

    init commit

---
+++ str_op.py
@@ -0,0 +1,9 @@
+s= "У лукоморья 123 дуб зеленый 456"
+s.find('я')
+s.index('я')
+s.count('у')
+if s.isalpha()== False:
+	s.upper()
+if len('У лукоморья 123 дуб зеленый 456') > 4:
+        s.lower()
+s.replace("У","О")
