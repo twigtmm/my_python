a86f64a2fe5efa7c4fd1fa84f075a5275deddf0a
Darya Popova <dasha_popova_1999@mail.ru>
2019-09-22 20:21:18 +0300

    init commit

---
+++ call_tel.py
@@ -0,0 +1,10 @@
+code= int(input("Введите код: "))
+time= int(input("Длительность переговоров: "))
+if code==343:
+          print("Екатеринбург, стоимость: ", 15*time)
+elif code== 381:
+    print("Омск, стоимость: ", 18*time)
+elif code==473:
+    print("Воронеж, стоимость: ",13*time)
+elif code==485:
+    print("Ярославль, стоимость: ", 11*time)
