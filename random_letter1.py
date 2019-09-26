3d125b4f752415f101706ba5aaf1d8b472fc2596
Darya Popova <dasha_popova_1999@mail.ru>
2019-09-26 12:46:16 +0300

    init commit

---
+++ random_letter1.py
@@ -0,0 +1,18 @@
+import random
+l=['самовар','весна','лето']
+a= random.choice(l)
+b=random.choice(a)
+lst=list(a)
+if b in lst:
+    c=lst.index(b)
+    lst.insert(c,'?')
+    lst.remove(b)
+    
+print(''.join(lst))
+d= input("Введите букву: ")
+if d==b:
+    print("Победа!")
+    print("Слово: ",a)
+else:
+    print("Увы! Попробуйте в другой раз")
+    print("Слово: ",a)
